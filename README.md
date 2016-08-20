cffccc12443d600627716c141cc6d2e62bd54297
ccmxy <windrainicefire@gmail.com>
2016-08-19 22:19:12 -0700

    Improved word replacement

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-Replaces all images in browser with this image of a unicorn:
+Replaces images in browser with this image of a unicorn:
 
 ![unicorn img] (./unicorn.png?raw=true "unicorn")
 
+It also replaces a few choice words in text on the page with different words.        
 Drag cloned folder into Chrome extensions to use.
--- content.js
+++ content.js
@@ -2,21 +2,17 @@ var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
-  //  if(element.nodeType = 1){
-    //  var isImg = (element.nodeName.toLowerCase() === 'img');
       if(isImage(element)){
         element.src="http://67.media.tumblr.com/30b1b0d0a42bca3759610242a1ff0348/tumblr_nnjxy1GQAA1tpo3v2o1_540.jpg";
-    //   alert(element.src);
      }
-  //  }
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
-
         if (node.nodeType === 3) {
             var text = node.nodeValue;
 
             var replacedText = text.replace(/wild/gi, 'rainbow bright');
-
+            replacedText = replacedText.replace(/tomorrow/gi, 'morrow');
+            replacedText = replacedText.replace(/ my| your| our/gi, ' the unicorn\'s');
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
