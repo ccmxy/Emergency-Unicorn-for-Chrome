b1ffa303e456b6329dc1c070b7fee7d2f1dc8192
ccmxy <windrainicefire@gmail.com>
2016-08-19 20:00:38 -0700

    It now replaces all images with the unicorn lol

--- content.js
+++ content.js
@@ -5,7 +5,8 @@ for (var i = 0; i < elements.length; i++) {
   //  if(element.nodeType = 1){
     //  var isImg = (element.nodeName.toLowerCase() === 'img');
       if(isImage(element)){
-       alert(element.src);
+        element.src="http://67.media.tumblr.com/30b1b0d0a42bca3759610242a1ff0348/tumblr_nnjxy1GQAA1tpo3v2o1_540.jpg";
+    //   alert(element.src);
      }
   //  }
     for (var j = 0; j < element.childNodes.length; j++) {
