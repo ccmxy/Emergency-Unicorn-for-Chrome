7092c5a1b7e733eb8fd60532c5ba0cb3b89d1750
ccmxy <windrainicefire@gmail.com>
2016-12-30 09:46:18 +0000

    It will now only change images and words upon clicking extension icon

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-Replaces images in browser with this image of a unicorn:
+#Emergency Unicorn
+
+Upon clicking the extension icon, all images in the browser window are replaced
+with this image of a unicorn:
 
 ![unicorn img] (./unicorn.png?raw=true "unicorn")
 
-It also replaces a few choice words in text on the page with different words.        
-Drag cloned folder into Chrome extensions to use.
+Simply refresh to turn the page back to its normal page.
+
+It also replaces a few choice words in text on the page with different words.      
+
+Drag cloned folder into Chrome extensions to use. A version of this extension
+that automatically replaces all images and words in the browser (without needing  
+to click the extension) is on [the branch auto-unicorn](/tree/auto-unicorn).
--- content.js
+++ content.js
@@ -1,25 +1,34 @@
-var elements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
+function showImages() {
+  var elements = document.getElementsByTagName('*');
+  for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
-      if(isImage(element)){
-        element.src="http://67.media.tumblr.com/30b1b0d0a42bca3759610242a1ff0348/tumblr_nnjxy1GQAA1tpo3v2o1_540.jpg";
-     }
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-
-            var replacedText = text.replace(/wild/gi, 'rainbow bright');
-            replacedText = replacedText.replace(/tomorrow/gi, 'morrow');
-            replacedText = replacedText.replace(/ my| your| our/gi, ' the unicorn\'s');
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
+    if (isImage(element)) {
+      element.src = "http://67.media.tumblr.com/30b1b0d0a42bca3759610242a1ff0348/tumblr_nnjxy1GQAA1tpo3v2o1_540.jpg";
     }
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+
+      if (node.nodeType === 3) {
+        var text = node.nodeValue;
+
+        var replacedText = text.replace(/wild/gi, 'rainbow bright');
+
+        if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+        }
+      }
+    }
+  }
 }
 
 function isImage(i) {
-    return i instanceof HTMLImageElement;
+  return i instanceof HTMLImageElement;
 }
+
+// Message handler
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.method === 'showImages') {
+    showImages();
+  }
+});
